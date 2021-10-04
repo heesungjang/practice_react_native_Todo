73c0be261ae95db2a00b98075968e5fa9a1ba572
Heesung jang <heesungj7@gmail.com>
2021-10-04 23:59:50 +0900

    textinput + props

--- App.js
+++ App.js
@@ -5,17 +5,29 @@ import {
     Text,
     View,
     TouchableOpacity,
-    TouchableHighlight,
-    TouchableWithoutFeedback,
-    Pressable,
+    TextInput,
 } from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
     const [working, setWorking] = useState(true);
+    const [text, setText] = useState("");
+    const [toDos, setToDos] = useState({});
     const travel = () => setWorking(false);
     const work = () => setWorking(true);
 
+    const onChangText = (payload) => setText(payload);
+    const addTodo = () => {
+        if (text === "") {
+            return;
+        }
+        // save to do
+        const newToDos = Object.assign({}, toDos, {
+            [Date.now()]: { text, work: working },
+        });
+        setToDos(newToDos);
+        setText("");
+    };
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
@@ -42,6 +54,17 @@ export default function App() {
                     </Text>
                 </TouchableOpacity>
             </View>
+
+            <TextInput
+                value={text}
+                returnKeyType="done"
+                onSubmitEditing={addTodo}
+                onChangeText={onChangText}
+                placeholder={
+                    working ? "Add a Todo" : "Where do you want to go?"
+                }
+                style={styles.input}
+            />
         </View>
     );
 }
@@ -60,5 +83,13 @@ const styles = StyleSheet.create({
     buttonText: {
         fontSize: 38,
         fontWeight: "600",
+    },
+    input: {
+        backgroundColor: "white",
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        borderRadius: 30,
+        marginTop: 20,
+        fontSize: 18,
     },
 });
