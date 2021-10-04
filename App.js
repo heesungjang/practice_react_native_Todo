1ce48b99f0eafa48d638a9af84b3edcf99f2bd92
Heesung jang <heesungj7@gmail.com>
2021-10-04 22:36:52 +0900

    touchable component 알아보기
    - TouchableOpacity,
    - TouchableHighlight,
    - TouchableWithoutFeedback

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    TouchableHighlight,
+    TouchableWithoutFeedback,
+    Pressable,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [selectedButton, setSelectedButton] = useState(null);
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+            <View style={styles.header}>
+                <TouchableOpacity>
+                    <Text style={styles.buttonText}>Work</Text>
+                </TouchableOpacity>
+                <Pressable
+                    onPress={() => {
+                        console.log("23");
+                    }}
+                >
+                    <Text style={styles.buttonText}>Travel</Text>
+                </Pressable>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: theme.bg,
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 100,
+        paddingHorizontal: 20,
+    },
+    buttonText: {
+        fontSize: 38,
+        fontWeight: "600",
+        color: theme.grey,
+    },
 });
